2d6acf7c231541a2dffb57c68aeb126065c80fe9
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-04-09 20:15:23 +0300

    Add project

---
+++ BuildingShop/BuildingShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BuildingShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
