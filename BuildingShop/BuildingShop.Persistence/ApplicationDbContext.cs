a21909ab02719b1f06eef6eb18cf47ef8bdbcc8a
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-05-23 16:11:58 +0300

    Add ApplicationDbContext

---
+++ BuildingShop/BuildingShop.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BuildingShop.Domain.DomainObjects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingShop.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
