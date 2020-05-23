38f921c6f1af786880fa679c6ca90389d452cb30
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-05-23 12:59:10 +0300

    Add model for Category

---
+++ BuildingShop/BuildingShop.Domain/DomainObjects/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingShop.Domain.DomainObjects
+{
+    public class Category: BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
