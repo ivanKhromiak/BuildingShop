398ad358ab52c419c57162e6bbc21167a58f2663
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-05-23 17:17:00 +0300

    Add ProductService

---
+++ BuildingShop/BuildingShop.BusinessLogic/Services/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using BuildingShop.Domain.DomainObjects;
+using BuildingShop.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BuildingShop.BusinessLogic.Services
+{
+    public class ProductService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return _context.Products.ToList();
+        }
+    }
+}
