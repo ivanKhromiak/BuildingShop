b6bfbc77b52ba8955a4d089b975243c9424de510
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-05-23 17:00:36 +0300

    Add seed data

---
+++ BuildingShop/BuildingShop.Persistence/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using BuildingShop.Domain.DomainObjects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingShop.Persistence.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void SeedData(this ModelBuilder builder)
+        {
+            builder.Entity<Category>().HasData(
+                new Category() { Id = 1, Name = "Instruments"},
+                new Category() { Id = 2, Name = "Light bulbs" }
+                );
+
+            builder.Entity<Product>().HasData(
+                new Product() { Id = 1, Name = "Hammer", Amount = 20, CategoryId = 1, Price = 50m },
+                new Product() { Id = 2, Name = "Diode lamp", Amount = 40, CategoryId = 2, Price = 25.99m }
+                );
+        }
+    }
+}
