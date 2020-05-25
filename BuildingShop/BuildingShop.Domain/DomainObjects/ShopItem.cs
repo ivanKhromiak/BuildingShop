9755ede74c4c058e4e48035ef043f057b5cbf02b
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-05-25 19:40:40 +0300

    Add ShopItem

---
+++ BuildingShop/BuildingShop.Domain/DomainObjects/ShopItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingShop.Domain.DomainObjects
+{
+    public class ShopItem : BaseEntity
+    {
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int Amount { get; set; }
+    }
+}
