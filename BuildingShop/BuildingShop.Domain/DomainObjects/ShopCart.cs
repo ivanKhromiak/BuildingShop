d008383ce8fa47d8c4948b07c92b4bae98b733aa
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-05-25 21:53:21 +0300

    Add shop cart

---
+++ BuildingShop/BuildingShop.Domain/DomainObjects/ShopCart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingShop.Domain.DomainObjects
+{
+    public class ShopCart : BaseEntity
+    {
+        public string SessionId { get; set; }
+
+        public List<ShopCartItem> ShopCartItems { get; set; }
+    }
+}
