779f3e2906a9c18811bf70286746f7dc459a0304
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-05-26 18:47:42 +0300

    Add configuration for Сharacteristics

--- BuildingShop/BuildingShop.Persistence/Configuration/ProductConfiguration.cs
+++ BuildingShop/BuildingShop.Persistence/Configuration/ProductConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using BuildingShop.Domain.DomainObjects;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,6 +13,12 @@ namespace BuildingShop.Persistence.Configuration
         public void Configure(EntityTypeBuilder<Product> builder)
         {
             builder.Property(p => p.Price).HasColumnType("decimal(12, 2)");
+            builder.Property(p => p.Сharacteristics)
+            .HasConversion(
+                d => JsonConvert.SerializeObject(d, Formatting.None),
+                s => JsonConvert.DeserializeObject<Dictionary<string, string>>(s)
+            )
+            .HasMaxLength(4000);
         }
     }
 }
