b5efd39336d5e38001d5abf4c049fc8a8af51334
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-05-27 21:18:09 +0300

    Add ShopCartController

---
+++ BuildingShop/BuildingShop.Web/Controllers/ShopCartController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BuildingShop.BusinessLogic.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildingShop.Web.Controllers
+{
+    public class ShopCartController : Controller
+    {
+        private readonly IShopCartService _shopCartService;
+
+        public ShopCartController(IShopCartService shopCartService)
+        {
+            _shopCartService = shopCartService;
+        }
+
+        public IActionResult Index()
+        {
+            string CartId = HttpContext.Session.GetString("CartId");
+            if (string.IsNullOrEmpty(CartId))
+            {
+                HttpContext.Session.SetString("CartId", Guid.NewGuid().ToString());
+                CartId = HttpContext.Session.GetString("CartId");
+            }
+            var items = _shopCartService.GetShopCartItems(CartId);
+            return View(items);
+        }
+
+        public IActionResult Buy(string CartId)
+        {
+            //shopCartService.Buy(CartId);
+            return RedirectToAction("Index");
+        }
+    }
+}
