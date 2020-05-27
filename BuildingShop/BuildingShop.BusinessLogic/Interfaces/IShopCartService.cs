37eff7eca52128110d334a0e4d9c96b8187c50d1
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-05-27 19:08:27 +0300

    Create ShopCartService

---
+++ BuildingShop/BuildingShop.BusinessLogic/Interfaces/IShopCartService.cs
@@ -0,0 +1,12 @@
+﻿using BuildingShop.Domain.DomainObjects;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BuildingShop.BusinessLogic.Interfaces
+{
+    public interface IShopCartService
+    {
+        Task AddToCart(Product product, int amount, string sessionId);
+        Task<List<ShopCartItem>> GetShopCartItems(string sessionId);
+    }
+}
---
+++ BuildingShop/BuildingShop.BusinessLogic/Services/ShopCartService.cs
@@ -0,0 +1,33 @@
+﻿using BuildingShop.BusinessLogic.Interfaces;
+using BuildingShop.Domain.DomainObjects;
+using BuildingShop.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingShop.BusinessLogic.Services
+{
+    public class ShopCartService : IShopCartService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ShopCartService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddToCart(Product product, int amount, string sessionId)
+        {
+            _context.ShopCartItems.Add(new ShopCartItem() { Product = product, Amount = amount, ShopCartId = sessionId });
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<ShopCartItem>> GetShopCartItems(string sessionId)
+        {
+            return await _context.ShopCartItems.Where(i => i.ShopCartId == sessionId).ToListAsync();
+        }
+    }
+}
