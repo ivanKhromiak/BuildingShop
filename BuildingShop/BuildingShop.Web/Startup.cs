9f9ae18bf735cbc1c8e053b02bf5fde5406524bb
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-05-27 21:17:40 +0300

    Register ShopCartService and delete unused usings in Startup

--- BuildingShop/BuildingShop.Web/Startup.cs
+++ BuildingShop/BuildingShop.Web/Startup.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -38,9 +32,10 @@ namespace BuildingShop.Web
             }); 
 
             services.AddTransient<IProductService, ProductService>();
+            services.AddTransient<IShopCartService, ShopCartService>();
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DataConnection")));
-
             services.AddDbContext<UsersDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("UsersConnection")));
 
