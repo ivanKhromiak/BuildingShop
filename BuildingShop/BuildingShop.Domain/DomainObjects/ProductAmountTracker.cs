1ceef8e2b2212c652c9a68bd953f21bc57757efb
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-05-30 13:28:21 +0300

    Add ProductAmountTracker

---
+++ BuildingShop/BuildingShop.Domain/DomainObjects/ProductAmountTracker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BuildingShop.Domain.DomainObjects
+{
+    public class ProductAmountTracker : BaseEntity
+    {
+        public int Amount { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
