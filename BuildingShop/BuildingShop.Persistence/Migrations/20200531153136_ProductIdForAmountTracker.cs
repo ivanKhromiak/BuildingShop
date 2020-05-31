9ca00f1f6e9b678d8e4b984228c85c99092d064e
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-05-31 19:56:20 +0300

    Add migration for ProductId in ProductAmoutTracker

---
+++ BuildingShop/BuildingShop.Persistence/Migrations/20200531153136_ProductIdForAmountTracker.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BuildingShop.Persistence.Migrations
+{
+    public partial class ProductIdForAmountTracker : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "ProductAmountTrackers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductAmountTrackers_ProductId",
+                table: "ProductAmountTrackers",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductAmountTrackers_Products_ProductId",
+                table: "ProductAmountTrackers",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductAmountTrackers_Products_ProductId",
+                table: "ProductAmountTrackers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductAmountTrackers_ProductId",
+                table: "ProductAmountTrackers");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "ProductAmountTrackers");
+        }
+    }
+}
