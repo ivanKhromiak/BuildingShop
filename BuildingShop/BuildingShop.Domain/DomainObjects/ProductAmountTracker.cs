9df702565711d7343ba3214ba24b021b2d785c59
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-05-31 18:29:18 +0300

    Add ProductId

--- BuildingShop/BuildingShop.Domain/DomainObjects/ProductAmountTracker.cs
+++ BuildingShop/BuildingShop.Domain/DomainObjects/ProductAmountTracker.cs
@@ -7,5 +7,9 @@ namespace BuildingShop.Domain.DomainObjects
         public int Amount { get; set; }
 
         public DateTime Date { get; set; }
+
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; }
     }
 }
