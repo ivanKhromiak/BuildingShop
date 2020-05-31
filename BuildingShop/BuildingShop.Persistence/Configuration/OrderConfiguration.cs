d3c600d4ae16d960c0becc069c476c956ff0f1cd
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-05-31 21:27:13 +0300

    Add OrderConfiguration

---
+++ BuildingShop/BuildingShop.Persistence/Configuration/OrderConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using BuildingShop.Domain.DomainObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BuildingShop.Persistence.Configuration
+{
+    class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.Property(o => o.AverageSalesPerDay).HasColumnType("decimal(12, 4)");
+        }
+    }
+}
