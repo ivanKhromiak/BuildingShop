a3b37855d9e5d784a7402d7d32d4ba07ea823b60
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-05-31 21:30:36 +0300

    Add migration for Order

--- BuildingShop/BuildingShop.Persistence/Migrations/20200531182907_AddOrder.Designer.cs
+++ BuildingShop/BuildingShop.Persistence/Migrations/20200531182907_AddOrder.Designer.cs
@@ -4,14 +4,16 @@ using BuildingShop.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BuildingShop.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200531182907_AddOrder")]
+    partial class AddOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BuildingShop/BuildingShop.Persistence/Migrations/20200531182907_AddOrder.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BuildingShop.Persistence.Migrations
+{
+    public partial class AddOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Purchases",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Deliveries",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(nullable: false),
+                    StarDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    StartingAmount = table.Column<int>(nullable: false),
+                    EndAmount = table.Column<int>(nullable: false),
+                    TotalIncome = table.Column<int>(nullable: false),
+                    TotalOutcome = table.Column<int>(nullable: false),
+                    MaxDaysWithoutProduct = table.Column<int>(nullable: false),
+                    AverageSalesPerDay = table.Column<decimal>(type: "decimal(12, 4)", nullable: false),
+                    MinSalePerDay = table.Column<int>(nullable: false),
+                    FinalNumber = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Purchases_OrderId",
+                table: "Purchases",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deliveries_OrderId",
+                table: "Deliveries",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ProductId",
+                table: "Orders",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Deliveries_Orders_OrderId",
+                table: "Deliveries",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Purchases_Orders_OrderId",
+                table: "Purchases",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Deliveries_Orders_OrderId",
+                table: "Deliveries");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Purchases_Orders_OrderId",
+                table: "Purchases");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Purchases_OrderId",
+                table: "Purchases");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Deliveries_OrderId",
+                table: "Deliveries");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Purchases");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Deliveries");
+        }
+    }
+}
