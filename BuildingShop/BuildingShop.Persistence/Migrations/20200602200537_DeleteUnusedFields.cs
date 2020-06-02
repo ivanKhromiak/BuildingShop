f5ed400c39bc8d104c7ccf64e24a5ac5113ebd7d
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-06-02 23:10:54 +0300

    Add migration for deleting Deliveries and Purchases

---
+++ BuildingShop/BuildingShop.Persistence/Migrations/20200602200537_DeleteUnusedFields.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BuildingShop.Persistence.Migrations
+{
+    public partial class DeleteUnusedFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Deliveries_Orders_OrderId",
+                table: "Deliveries");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Purchases_Orders_OrderId",
+                table: "Purchases");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Purchases_OrderId",
+                table: "Purchases");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Deliveries_OrderId",
+                table: "Deliveries");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Purchases");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Deliveries");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Purchases",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Deliveries",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Purchases_OrderId",
+                table: "Purchases",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deliveries_OrderId",
+                table: "Deliveries",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Deliveries_Orders_OrderId",
+                table: "Deliveries",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Purchases_Orders_OrderId",
+                table: "Purchases",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
