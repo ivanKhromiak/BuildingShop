35e2201b0bcbe36cc21cb31d65ccafad576f6e5b
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-06-02 22:31:17 +0300

    improve CreateOrder

--- BuildingShop/BuildingShop.BusinessLogic/Services/OrderService.cs
+++ BuildingShop/BuildingShop.BusinessLogic/Services/OrderService.cs
@@ -30,41 +30,46 @@ namespace BuildingShop.BusinessLogic.Services
 
         public async Task CreateOrder(Order order)
         {
-            order.Deliveries = _context.Deliveries
+            order.Deliveries = await _context.Deliveries
                 .Where(d => d.Date <= order.EndDate && d.Date >= order.StarDate)
-                .ToList();
-            order.Purchases = _context.Purchases
+                .ToListAsync();
+            order.Purchases = await _context.Purchases
                 .Where(d => d.Date <= order.EndDate && d.Date >= order.StarDate)
-                .ToList();
+                .ToListAsync();
 
-            order.TotalIncome = order.Purchases.Sum(d => d.Amount);
-            order.TotalOutcome = order.Deliveries.Sum(d => d.Amount);
+            order.TotalIncome = order.Deliveries.Sum(d => d.Amount);
+            order.TotalOutcome = order.Purchases.Sum(d => d.Amount);
 
-            order.StartingAmount = _context.ProductAmountTrackers.Where(a => a.Date < order.StarDate).LastOrDefault().Amount;
+            order.StartingAmount = (await _context.ProductAmountTrackers
+                .Where(a => order.StarDate > a.Date)
+                .OrderBy(a => a.Date)
+                .ToListAsync())
+                .LastOrDefault()
+                .Amount;
             order.EndAmount = order.StartingAmount + order.TotalIncome - order.TotalOutcome;
 
             var currentDate = order.StarDate;
             int currentAmount = order.StartingAmount;
 
-            order.MinSalePerDay = order.Purchases.Min(p => p.Amount);
-
             while (currentDate <= order.EndDate)
             {
-                currentAmount += order.Deliveries.FirstOrDefault(d => d.Date == currentDate)?.Amount ?? 0
-                    - order.Purchases.FirstOrDefault(d => d.Date == currentDate)?.Amount ?? 0;
-                if(currentAmount == 0)
+                currentAmount += order.Deliveries.FirstOrDefault(d => d.Date == currentDate)?.Amount ?? 0;
+                if (currentAmount == 0)
                 {
                     order.DaysWithoutProduct++;
                 }
-                currentDate.AddDays(1);
+                currentAmount -= order.Purchases.FirstOrDefault(d => d.Date == currentDate)?.Amount ?? 0;
+                currentDate = currentDate.AddDays(1);
             }
+
+            order.MinSalePerDay = order.Purchases.Min(p => p.Amount);
 
             order.AverageSalesPerDay = (decimal)(order.TotalOutcome / (order.EndDate - order.StarDate).TotalDays);
 
             order.FinalNumber = (int)(order.AverageSalesPerDay * (decimal)(order.EndDate - order.StarDate).TotalDays);
 
-            _context.Orders.Add(order);
-            _context.SaveChanges();
+           await _context.Orders.AddAsync(order);
+           await _context.SaveChangesAsync();
         }
 
         public async Task DeleteOrder(int orderId)
