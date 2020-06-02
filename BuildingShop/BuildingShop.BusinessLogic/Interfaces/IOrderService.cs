f1f4b12555d2506e143ee45dd46fbbecc19d921f
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-06-02 18:44:22 +0300

    Add IOrderService

---
+++ BuildingShop/BuildingShop.BusinessLogic/Interfaces/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using BuildingShop.Domain.DomainObjects;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BuildingShop.BusinessLogic.Interfaces
+{
+    public interface IOrderService
+    {
+        Task CreateOrder(Order order);
+        Task DeleteOrder(int orderId);
+        Task<List<Order>> GetAllOrders();
+        Task<Order> GetOrder(int orderId);
+    }
+}
