597bd4b61ab70d16a3f9b238254ac39309e1a434
Ivan Khromiak <ivan.khromiak@gmail.com>
2020-06-03 21:12:09 +0300

    add OrderViewModel

---
+++ BuildingShop/BuildingShop.Web/Models/OrderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BuildingShop.Domain.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildingShop.Web.Models
+{
+    public class OrderViewModel
+    {
+        public Order Order { get; set; }
+        public List<Delivery> Deliveries { get; set; }
+        public List<Purchase> Purchases { get; set; }
+    }
+}
